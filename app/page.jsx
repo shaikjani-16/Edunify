95f859d28b0c33e86c85561edc8ecdb6f4ebb3f8
shaikjani-16 <106083984+shaikjani-16@users.noreply.github.com>
2024-07-25 04:12:53 +0530

    final code

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,8 @@
-import { uploadFile } from "./actions/file.js";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <form action={uploadFile}>
-          <input type="file" name="profile-image" />
-          <input
-            type="submit"
-            value="upload-image"
-            className="border-2 p-2 border-black rounded"
-          />
-        </form>
+        <h1>Hi</h1>
       </div>
     </main>
   );
